ce6e50148b1ac6db49f41f193efb3f900b2904a4
jiquanxi <jiquanxi@pxjy.com>
2020-07-20 15:24:55 +0800

    add spring web RestTemplate to client;

--- src/main/java/com/github/tinyurl/client/http/RestClient.java
+++ src/main/java/com/github/tinyurl/client/http/RestClient.java
@@ -2,13 +2,13 @@ package com.github.tinyurl.client.http;
 
 import com.alibaba.fastjson.JSON;
 import com.github.tinyurl.client.config.HttpPoolConfig;
-import com.github.tinyurl.client.loadbalancer.BaseLoadBalancer;
 import com.github.tinyurl.client.loadbalancer.LoadBalancer;
+import com.github.tinyurl.client.util.ObjectUtil;
 import com.github.tinyurl.client.util.StringUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.util.Map;
 
 /**
  * http客户端接口
@@ -22,11 +22,14 @@ public class RestClient {
 
     private HttpClientPool httpClientPool;
 
+    private RestTemplate restTemplate;
+
     private final LoadBalancer loadBalancer;
 
-    public RestClient(HttpPoolConfig httpPoolConfig, LoadBalancer loadBalancer) {
+    public RestClient(HttpPoolConfig httpPoolConfig, LoadBalancer loadBalancer, RestTemplate restTemplate) {
         this.httpPoolConfig = httpPoolConfig;
         this.loadBalancer = loadBalancer;
+        this.restTemplate = restTemplate;
     }
 
     public void initialize() {
@@ -36,19 +39,23 @@ public class RestClient {
     /**
      * POST请求接口
      * @param url 接口URL
-     * @param params 接口参数
+     * @param param 接口参数
      * @param classObj
      * @param <T>
      * @return
      */
     public <T> T postForObject(String url, Object param, Class<T> classObj) {
-        try {
-            String result = httpClientPool.post(url, param);
-            if (StringUtil.isNotEmpty(result)) {
-                return JSON.parseObject(result, classObj);
+        if (ObjectUtil.isNotNull(restTemplate)) {
+            return restTemplate.postForObject(url, param, classObj);
+        } else {
+            try {
+                String result = httpClientPool.post(url, param);
+                if (StringUtil.isNotEmpty(result)) {
+                    return JSON.parseObject(result, classObj);
+                }
+            } catch (IOException | InterruptedException e) {
+                log.error("error", e);
             }
-        } catch (IOException | InterruptedException e) {
-            log.error("error", e);
         }
 
         return null;
